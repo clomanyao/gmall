a8239a9a4cdf8a837f558a4d3388da4d22883c98
2482118722ysj <2482118722@qq.com>
2020-05-08 22:39:37 +0800

    项目初始结构创建

---
+++ gmall-member/src/test/java/com/macroyao/gmall/lmember/GmallMemberApplicationTests.java
@@ -0,0 +1,13 @@
+package com.macroyao.gmall.lmember;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallMemberApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
